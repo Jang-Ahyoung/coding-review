46431e047046d17cb4a8e094c8249e3ec21b8815
Jang-Ahyoung <wegotthelove27@gmail.com>
2021-04-17 19:43:29 +0900

    재수정

--- backjoon/다이나믹프로그래밍/계단 오르기.py
+++ backjoon/다이나믹프로그래밍/계단 오르기.py
@@ -1,3 +1,41 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+dp = [0 for _ in range(N+3)]
+arr = [0 for _ in range(N+3)]
+for k in range(1,N+1):
+    arr[k] = int(input())
+
+
+dp [1] = arr[1]
+dp [2] = arr[1] + arr[2]
+dp [3] = max(arr[1] + arr[3] ,arr[2] + arr[3])
+
+
+for i in range(4, N+1):
+    dp[i] = max( dp[i-3] + arr[i-1] + arr[i] ,  dp[i-2] + arr[i] ) 
+print(dp[N])
+
+
+import sys
+input = sys.stdin.readline
+n=int(input())
+stairs=[0]*300  # 변수 n 입력시 런타임 에러떠
+
+for i in range(n):
+  stairs[i]=int(input())
+
+d=[0]*300
+d[0]=stairs[0]
+d[1]=stairs[0]+stairs[1]
+d[2]=max(stairs[2]+stairs[0], stairs[2]+stairs[1])
+
+for i in range(3,n):
+  d[i]=max(stairs[i]+d[i-2], stairs[i]+stairs[i-1]+d[i-3])
+print(d[n-1])
+
+
 from sys import stdin
 
 a,b,c = 0,0,0
@@ -9,25 +47,3 @@ for _ in range(n):
     a,b,c = d_0,d_1,d_2
 
 print(max(d_2,d_1))
-
-
-
-#런타임 에러
-import sys 
-input = sys.stdin.readline 
-arr = [] 
-dp = []
-
-l = int(input()) 
-for _ in range(l): 
-  arr.append(int(input()))
-
-dp.append(arr[0]) # 1올라
-dp.append(max(arr[0]+arr[1],arr[1])) # 1,1 올르거나 2오른 가중치
-dp.append(max(arr[0]+arr[2],arr[1]+arr[2])) #1,2 오르거나 2,1오른 가중치
-
-for i in range(3,l):
-  # i번 째 계단까지의 최대 가중치 합
-  dp.append(max(dp[i-2]+arr[i],dp[i-3]+arr[i]+arr[i-1])) 
-  # i -2까지의 최대 가중치 합 + 현재 계단의 가중 치  VS i-3까지의 최대 가중치 합 + i-1의 가중치 + 현재 계단의 가중 치
-print(dp.pop())
