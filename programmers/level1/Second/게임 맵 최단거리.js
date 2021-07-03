b823998531abda59fd11aed99a1c7c8469300743
Jang-Ahyoung <wegotthelove27@gmail.com>
2021-07-03 11:40:36 +0900

    2021-07-03

--- programmers/level1/Second/게임 맵 최단거리.js
+++ programmers/level1/Second/게임 맵 최단거리.js
@@ -1,3 +1,4 @@
+// bfs를 활용한 너비 탐색
 function solution(maps) {
     // 동서남북
     const dy = [0, 0, 1, -1];
@@ -26,4 +27,38 @@ function solution(maps) {
         }
     }
     return visitCount[row - 1][col - 1] === 1 ? -1 : visitCount[row - 1][col - 1];
+}
+
+function solution(maps) {
+    // 동서남북 이동할 값 배열 저장
+    const dy = [0, 0, 1, -1];
+    const dx = [1, -1, 0, 0];
+    const row = maps.length;
+    const col = maps[0].length;
+    const visitedCount = [...maps].map((node) => node.map((num) => 1)); // 각 열의 각 번호 1로 초기화
+
+    let temp = 0;
+    const queue = [[0, 0]]; // 0,0부터 시작!
+
+    while (queue.length) {
+        const [y, x] = queue.shift(); // queue의 값 선출선입해서 할당!
+        for (let i = 0; i < 4; i++) {
+            const ny = y + dy[i];
+            const nx = x + dx[i];
+
+            if (ny >= 0 && nx >= 0 && ny < row && nx < col) {
+                if (maps[ny][nx] === 1 && visitedCount[ny][nx] === 1) {
+                    visitedCount[ny][nx] = visitedCount[y][x] + 1;
+                    queue.push([ny, nx]);
+                }
+            }
+
+        }
+    }
+
+    return visitedCount[row - 1][col - 1] === 1 ? -1 : visitedCount[row - 1][col - 1];
+
+
+
+
 }
---
+++ programmers/level1/Second/네트워크.js
@@ -0,0 +1,25 @@
+// bfs를 활용한 깊이 탐색
+// 컴퓨터 연결되 있을경우 1로 표현
+// 방문기록이 없는 노드를 기준으로 다시 DFS, BFS를 한다는 생각(반복문 이용)으로 문제에 접근
+function solution(n, computers) {
+    let answer = 0;
+    const queue = [];
+    const visited = {}; // undefinded / false : Array(n).fill(false); 
+    for (let i = 0; i < n; i++) {
+        if (!visited[i]) { // 미방문 노드의 경우 큐에 넣어준 후 방문 처리해준다
+            queue.push(i);
+            visited[i] = true;
+            answer++;
+        }
+        while (queue.length) {
+            let node = queue.shift();
+            computers[node].forEach((value, idx) => {
+                if (!visited[idx] && value === 1) { // 미방문 && 연결되어있으면
+                    visited[idx] = true;
+                    queue.push(idx);
+                }
+            });
+        }
+    }
+    return answer;
+}
