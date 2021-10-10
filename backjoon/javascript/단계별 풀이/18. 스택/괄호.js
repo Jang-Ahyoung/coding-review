19641b5813bfffec7b0b0dde6ac82cd5898d5a73
Jang-Ahyoung <wegotthelove27@gmail.com>
2021-10-10 22:44:38 +0900

    2021-10-10

---
+++ backjoon/javascript/단계별 풀이/18. 스택/괄호.js
@@ -0,0 +1,15 @@
+// 올바른 괄호 문자열(VPS) 판단해 결과 YES 와 NO로 리턴
+
+// 1. replace 사용
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+let len = input.shift();
+let result = '';
+
+for (let i = 0; i < len; i++) {
+    let inputStr = input[i];
+    while (inputStr.indexOf("()") >= 0) {
+        inputStr = inputStr.replace("()", "");
+    }
+
+    console.log(inputStr === '' ? 'YES' : 'NO');
+}
